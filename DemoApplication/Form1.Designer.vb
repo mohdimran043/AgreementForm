766817baf93c08d9290f004f465eadfb00d0a7fe
imranm@aditi.com <mohdimran043@gmail.com>
2017-06-13 22:15:15 +0300

    First sample

---
+++ DemoApplication/Form1.Designer.vb
@@ -0,0 +1,111 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
+        Me.WebBrowser1 = New System.Windows.Forms.WebBrowser()
+        Me.TableLayoutPanel2 = New System.Windows.Forms.TableLayoutPanel()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.TableLayoutPanel1.SuspendLayout()
+        Me.TableLayoutPanel2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TableLayoutPanel1
+        '
+        Me.TableLayoutPanel1.ColumnCount = 1
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
+        Me.TableLayoutPanel1.Controls.Add(Me.WebBrowser1, 0, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.TableLayoutPanel2, 0, 1)
+        Me.TableLayoutPanel1.Location = New System.Drawing.Point(12, 12)
+        Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
+        Me.TableLayoutPanel1.RowCount = 2
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 87.70686!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.29314!))
+        Me.TableLayoutPanel1.Size = New System.Drawing.Size(976, 423)
+        Me.TableLayoutPanel1.TabIndex = 0
+        '
+        'WebBrowser1
+        '
+        Me.WebBrowser1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.WebBrowser1.Location = New System.Drawing.Point(3, 3)
+        Me.WebBrowser1.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.WebBrowser1.Name = "WebBrowser1"
+        Me.WebBrowser1.Size = New System.Drawing.Size(970, 365)
+        Me.WebBrowser1.TabIndex = 0
+        '
+        'TableLayoutPanel2
+        '
+        Me.TableLayoutPanel2.ColumnCount = 2
+        Me.TableLayoutPanel2.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 83.79829!))
+        Me.TableLayoutPanel2.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 186.0!))
+        Me.TableLayoutPanel2.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.20172!))
+        Me.TableLayoutPanel2.Controls.Add(Me.Button1, 1, 0)
+        Me.TableLayoutPanel2.Controls.Add(Me.CheckBox1, 0, 0)
+        Me.TableLayoutPanel2.Location = New System.Drawing.Point(3, 374)
+        Me.TableLayoutPanel2.Name = "TableLayoutPanel2"
+        Me.TableLayoutPanel2.RowCount = 1
+        Me.TableLayoutPanel2.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 39.13044!))
+        Me.TableLayoutPanel2.Size = New System.Drawing.Size(970, 46)
+        Me.TableLayoutPanel2.TabIndex = 1
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(787, 3)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(173, 40)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(3, 3)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(116, 24)
+        Me.CheckBox1.TabIndex = 1
+        Me.CheckBox1.Text = "CheckBox1"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1000, 447)
+        Me.Controls.Add(Me.TableLayoutPanel1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.TableLayoutPanel1.ResumeLayout(False)
+        Me.TableLayoutPanel2.ResumeLayout(False)
+        Me.TableLayoutPanel2.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents TableLayoutPanel1 As System.Windows.Forms.TableLayoutPanel
+    Friend WithEvents WebBrowser1 As System.Windows.Forms.WebBrowser
+    Friend WithEvents TableLayoutPanel2 As System.Windows.Forms.TableLayoutPanel
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents CheckBox1 As System.Windows.Forms.CheckBox
+
+End Class
---
+++ DemoApplication/Form1.vb
@@ -0,0 +1,128 @@
+﻿Imports System.Runtime.InteropServices
+
+Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs)
+        Me.Close()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.TopMost = True
+        Me.ShowInTaskbar = False
+        FormBorderStyle = FormBorderStyle.None
+        WindowState = FormWindowState.Maximized
+        TableLayoutPanel1.Width = Me.Width
+        TableLayoutPanel1.Height = Me.Height
+        WebBrowser1.Width = Me.Width
+        TableLayoutPanel2.Width = Me.Width
+        'KeyboardHook(Me, e)
+        ' EnableWindow(1, False)
+        'Me.ShowInTaskbar = True
+        ' MyFilter.SuppressKeys = Not MyFilter.SuppressKeys
+
+    End Sub
+
+    Protected Overrides Function ProcessDialogKey(keyData As Keys) As Boolean
+        Return False
+    End Function
+    Private Delegate Function LowLevelKeyboardProcDelegate(nCode As Integer, wParam As Integer, ByRef lParam As KBDLLHOOKSTRUCT) As Integer
+
+    <DllImport("user32.dll", EntryPoint:="SetWindowsHookExA", CharSet:=CharSet.Ansi)> _
+    Private Shared Function SetWindowsHookEx(idHook As Integer, lpfn As LowLevelKeyboardProcDelegate, hMod As Integer, dwThreadId As Integer) As Integer
+    End Function
+
+    <DllImport("user32.dll")> _
+    Private Shared Function UnhookWindowsHookEx(hHook As Integer) As Integer
+    End Function
+
+    <DllImport("user32.dll", EntryPoint:="CallNextHookEx", CharSet:=CharSet.Ansi)> _
+    Private Shared Function CallNextHookEx(hHook As Integer, nCode As Integer, wParam As Integer, ByRef lParam As KBDLLHOOKSTRUCT) As Integer
+    End Function
+
+    Const WH_KEYBOARD_LL As Integer = 13
+    Private intLLKey As Integer
+    Private lParam As KBDLLHOOKSTRUCT
+
+    Private Structure KBDLLHOOKSTRUCT
+        Public vkCode As Integer
+        Private scanCode As Integer
+        Public flags As Integer
+        Private time As Integer
+        Private dwExtraInfo As Integer
+    End Structure
+    <DllImport("user32.dll")> _
+    Private Shared Function EnableWindow(hWnd As IntPtr, enable As Boolean) As Boolean
+    End Function
+    Private Function LowLevelKeyboardProc(nCode As Integer, wParam As Integer, ByRef lParam As KBDLLHOOKSTRUCT) As Integer
+        Dim blnEat As Boolean = False
+        Select Case wParam
+            Case 256, 257, 260, 261
+                'Alt+Tab, Alt+Esc, Ctrl+Esc, Windows Key
+                If ((lParam.vkCode = 9) AndAlso (lParam.flags = 32)) OrElse ((lParam.vkCode = 27) AndAlso (lParam.flags = 32)) OrElse ((lParam.vkCode = 27) AndAlso (lParam.flags = 0)) OrElse ((lParam.vkCode = 91) AndAlso (lParam.flags = 1)) OrElse ((lParam.vkCode = 92) AndAlso (lParam.flags = 1)) OrElse ((True) AndAlso (lParam.flags = 32)) Then
+                    blnEat = True
+                End If
+                Exit Select
+        End Select
+
+        If blnEat Then
+            Return 1
+        Else
+            Return CallNextHookEx(0, nCode, wParam, lParam)
+        End If
+
+    End Function
+
+    Private Sub KeyboardHook(sender As Object, e As EventArgs)
+        intLLKey = SetWindowsHookEx(WH_KEYBOARD_LL, New LowLevelKeyboardProcDelegate(AddressOf LowLevelKeyboardProc), System.Runtime.InteropServices.Marshal.GetHINSTANCE(System.Reflection.Assembly.GetExecutingAssembly().GetModules()(0)).ToInt32(), 0)
+    End Sub
+
+    Private Sub ReleaseKeyboardHook()
+        intLLKey = UnhookWindowsHookEx(intLLKey)
+    End Sub
+
+    Private Sub webBrowser1_PreviewKeyDown(sender As Object, e As PreviewKeyDownEventArgs)
+        KeyboardHook(Me, e)
+    End Sub
+
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles Button1.Click
+        Me.Close()
+    End Sub
+
+    Private Sub TableLayoutPanel1_Paint(sender As Object, e As PaintEventArgs) Handles TableLayoutPanel1.Paint
+
+    End Sub
+End Class
+
+Public Class MyFilter
+    Implements IMessageFilter
+
+    Public Shared SuppressKeys As Boolean = False
+
+    Private Const WM_KEYDOWN As Integer = &H100
+    Private Const WM_KEYUP As Integer = &H101
+
+    Public Function PreFilterMessage(ByRef m As Message) As Boolean Implements IMessageFilter.PreFilterMessage
+        Select Case m.Msg
+            Case WM_KEYDOWN, WM_KEYUP
+                If MyFilter.SuppressKeys Then
+                    Return True
+                End If
+                Exit Select
+            Case Else
+
+                Exit Select
+        End Select
+        Return False
+    End Function
+
+End Class
+
+Partial Public Class NativeMethods
+
+    ''' Return Type: BOOL->int
+    '''fBlockIt: BOOL->int
+    <System.Runtime.InteropServices.DllImportAttribute("user32.dll", EntryPoint:="BlockInput")> _
+    Public Shared Function BlockInput(<System.Runtime.InteropServices.MarshalAsAttribute(System.Runtime.InteropServices.UnmanagedType.Bool)> fBlockIt As Boolean) As <System.Runtime.InteropServices.MarshalAsAttribute(System.Runtime.InteropServices.UnmanagedType.Bool)> Boolean
+    End Function
+
+End Class
